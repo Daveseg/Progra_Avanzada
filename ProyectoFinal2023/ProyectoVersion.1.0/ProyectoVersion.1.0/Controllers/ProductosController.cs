fdd8fbb3bf295019405dd6dfdd15d1a0b6c5c292
Andresalas127 <106887514+Andresalas127@users.noreply.github.com>
2023-11-12 01:16:27 -0600

    Actualiza Controlador, Vistas y Layout
    Hice un Layout para todos los productos.
    Hice una vista para cada categoría
    Hice los métodos para que devuelva cada una de las vistas
    La página no se cae en ningún apartado, al presionar "Suplementicos los lleva a Home, desde cualquier página pueden ingresar a cualquier categoría.

--- ProyectoFinal2023/ProyectoVersion.1.0/ProyectoVersion.1.0/Controllers/ProductosController.cs
+++ ProyectoFinal2023/ProyectoVersion.1.0/ProyectoVersion.1.0/Controllers/ProductosController.cs
@@ -13,5 +13,40 @@ namespace ProyectoVersion._1._0.Controllers
         {
             return View();
         }
+        [HttpGet]
+        public ActionResult Shakers()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Accesorios()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult ProteinasPolvo()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult ProteinasSuero()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult ProteinasVegetal()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult SuplementosAdultos()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult SuplementosDeportivos()
+        {
+            return View();
+        }
     }
 }
